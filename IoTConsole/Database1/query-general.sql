917425a6da5a03642ae496314be36da8c77350b1
julee <julee@microsoft.com>
2016-09-26 10:29:56 +0900

    a few

--- IoTConsole/Database1/query-general.sql
+++ IoTConsole/Database1/query-general.sql
@@ -8,3 +8,21 @@ set ElapsedTime=datediff(ms,ServiceSendTime,LogCreatedTime)
 where datediff(SECOND,LogCreatedTime,getdate()) < 6500
 
 select iothubreceivetime,logcreatedtime,ElapsedTime,description from PerfLogs where deviceid='demo' and Description='single_no_wait_no_window_on_ASA'
+select iothubreceivetime,logcreatedtime,ElapsedTime,description from PerfLogs where deviceid='demo' and Description='stream analytics'
+select 
+	description,
+	count(distinct logcreatedtime) as [number of DB trans], 
+	count(logcreatedtime) as [number of trans], 
+	count(logcreatedtime)/datediff(second,min(logcreatedtime),max(logcreatedtime)) as [trans per sec], 
+	Avg(ElapsedTime) as [average elapsed time(ms)],
+	min(ElapsedTime) as [min elapsed time(ms)],
+	max(elapsedtime) as [max elapsed time(ms)]
+	from PerfLogs 
+	where deviceid='demo' 
+	group by Description
+
+select datediff(second,min(logcreatedtime),max(logcreatedtime)) from PerfLogs
+
+delete from PerfLogs where description='insert test'
+
+select distinct description from PerfLogs
