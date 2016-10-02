87b0dc950b2b75c1f4b494db332b50ed721a1ca7
julee <julee@microsoft.com>
2016-10-02 21:20:07 +0900

    sql

--- IoTConsole/Database1/query-general.sql
+++ IoTConsole/Database1/query-general.sql
@@ -27,10 +27,39 @@ select datediff(second,min(logcreatedtime),max(logcreatedtime)) from PerfLogs wh
 select count(logcreatedtime) from PerfLogs where deviceid='su1'
 select count(logcreatedtime)/datediff(second,min(logcreatedtime),max(logcreatedtime)) as [trans per sec] from PerfLogs where deviceid='demo3'
 
-delete from PerfLogs where description='insert test'
+delete from PerfLogs where DeviceSDKversion='2 partition 1 device'
 
 select distinct description from PerfLogs
 
 select * from PerfLogs where deviceid='a' or deviceid='c'
 
 delete from PerfLogs where deviceid='32part'
+
+--check performance
+Declare @StartTime datetime2
+SET @StartTime = (select top 1 servicesendtime from PerfLogs where datediff(minute,Devicetime,getdate())<60 order by ServiceSendTime desc)
+
+Declare @DeviceSDKVersion nvarchar(50)
+SET @DeviceSDKVersion = (select top 1 DeviceSDKVersion from PerfLogs where ServiceSendTime=@StartTime and Description='VM')
+
+UPDATE PerfLogs
+set DeviceSDKversion=@DeviceSDKVersion
+where Description='Stream Analytics' and ServiceSendTime=@StartTime
+
+UPDATE PerfLogs
+set E2ETime=datediff(ms,DeviceTime,LogCreatedTime)
+where DeviceSDKversion=@DeviceSDKVersion
+
+select 
+	DeviceSDKversion,
+	description,
+	count(distinct logcreatedtime) as [number of DB trans], 
+	count(logcreatedtime) as [number of trans], 
+	count(logcreatedtime)/datediff(second,min(logcreatedtime),max(logcreatedtime)) as [trans per sec], 
+	Avg(E2ETime) as [average E2E time(ms)],
+	min(E2ETime) as [min E2E time(ms)],
+	max(E2ETime) as [max E2E time(ms)],
+	avg(elapsedtime) as [average VM sercice time(ms)]
+	from PerfLogs 
+	where DeviceSDKversion=@DeviceSDKVersion
+	group by Description, DeviceSDKversion
